cc421518b2a2192b46875f6b64ab8f7f9ca21e08
Jessica Koscheka <jesskos@gmail.com>
2018-07-13 23:16:49 +0200

    completed urlify problem to convert string

--- cracking_the_coding_interview_problems.py
+++ cracking_the_coding_interview_problems.py
@@ -6,3 +6,13 @@ def all_unique(str):
 			return False
 	return True 
 
+def is_permutation_of_word(word1, word2):
+	pass 
+
+def urlify(phrase):
+	empty_str = ""
+	''' replaces all strings in a phrase with %20 ''' 
+	phrase_list = phrase.split(" ")
+	return "%20".join(phrase_list)
+
+
