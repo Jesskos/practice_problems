3c44d5d8ef16f18c84ec52fb0f5a9e5244d97eba
Jessica Koscheka <jesskos@gmail.com>
2018-07-15 15:15:00 +0200

    completed is_permutation_of_word

--- cracking_the_coding_interview_problems.py
+++ cracking_the_coding_interview_problems.py
@@ -7,7 +7,20 @@ def all_unique(str):
 	return True 
 
 def is_permutation_of_word(word1, word2):
-	pass 
+	''' checks if two words are permutations of eachother '''
+	if word1 == word2:
+		return False
+	letter_count_dictionary_word1= {}
+	letter_count_dictionary_word2= {}
+	for letter in word1:
+		letter_count_dictionary_word1[letter] = letter_count_dictionary_word1.get(letter, 0) + 1 
+	for letter in word2:
+		letter_count_dictionary_word2[letter] = letter_count_dictionary_word2.get(letter, 0) + 1
+	if letter_count_dictionary_word1 != letter_count_dictionary_word2:
+		return False
+	return True 
+
+
 
 def urlify(phrase):
 	empty_str = ""
