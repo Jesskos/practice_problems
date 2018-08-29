4f67df28c5f80e9dbc8e86310bfb4c462a27f3ee
Jessica Koscheka <jesskos@gmail.com>
2018-08-29 16:02:30 -0700

    made a stack using methods from a queue class

--- QueuesAndStacks.py
+++ QueuesAndStacks.py
@@ -90,6 +90,55 @@ class MinStack(object):
     		return -1
 
 
+class Queue(object):
+	''' a simple queue made using a regular Python list. Although a Linked List is preferable, this is made for ease'''
+	def __init__(self):
+		self._queue = []
+
+	def enqueue(self, item):
+		self._queue.append(item)
+
+	def dequeue(self):
+		if self._queue:
+			self._queue.pop(0)
+
+	def front(self):
+		if self.print_queue:
+			return self._queue[0]
+
+	def is_empty(self):
+		if not self._queue:
+			return True
+		else:
+			return False
+
+	def print_queue(self):
+		print self._queue
+
+class StackQueue(object):
+	''' A stack made from using queue methods '''
+
+	def __init__(self):
+		self.my_stack_push = Queue()
+		self.my_stack_pop = Queue()
+
+
+	def push(self, item):
+		self.my_stack_push.enqueue(item)
+
+	def pop(self):
+		if self.my_stack_pop.is_empty():
+			item = self.my_stack_push.dequeue()
+			self.my_stack_pop.enqueue(item)
+		popped_item = self.my_stack_pop.dequeue()
+		return popped_item
+
+
+	def print_stack(self):
+		print self.my_stack_push.print_queue()
+		print self.my_stack_pop.print_queue()
+
+
 
 
 
