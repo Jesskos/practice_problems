00316f17a6d7b6ac11d5a7eca3d25d610559108c
Jessica Koscheka <jesskos@gmail.com>
2018-09-06 10:55:13 -0700

    worked on make_best

--- Trees.py
+++ Trees.py
@@ -1,1 +1,37 @@
+class BinaryNode(object):
+    """Node in a binary tree."""
 
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+
+def make_bst(nums):
+    """Given a list of sorted numbers, make a binary search tree.
+
+    Returns the root node of a new BST that is valid and balanced.
+
+
+    """
+
+    if not nums:
+    	return None
+
+    midpoint = len(nums)/2
+    node = BinaryNode(nums[midpoint])
+
+    node.left = make_bst(nums[:midpoint])
+    node.right = make_bst(nums[midpoint+1:])
+
+    return node
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print("\n**** ALL TESTS PASSED. YOU'RE A TREE-MASTER!\n")
